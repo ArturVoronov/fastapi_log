581c574cc95ffb6280d770e1b9b75c3a041972ae
Artur Voronov <artur.voronov.1979@mail.ru>
2025-05-08 11:26:38 +0300

    first commit

---
+++ auth_routers.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, status
+from database import Session, engine
+from models import User
+from schemas import SignUpModel
+from fastapi.exceptions import HTTPException
+from werkzeug.security import generate_password_hash, check_password_hash
+from schemas import SignUpModel
+
+auth_router=APIRouter( prefix='/auth', tags=['auth'])
+
+session = Session(bind=engine)
+
+@auth_router.get('/')
+async def hello():
+    return {"message":"hello Artyr"}
+
+@auth_router.post('/signup')
+async def signup(users:SignUpModel):
+    print('1')
+    db_email=session.query(User).filter(User.email==users.email).first()
+    if db_email is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with the email already exists")
+    print('2')
+    db_username=session.query(User).filter(User.username==users.username).first()
+    print('3')
+    if db_username is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with the username already exists")
+    print('4')
+    new_user=User(
+        username=users.username,
+        email=users.email,
+        password=generate_password_hash(users.password),
+        is_active=users.is_active,
+        is_staff=users.is_staff
+    )
+    print(new_user)
+    session.add(new_user)
+    session.commit()
+    return new_user
+def create_users():
+    
+    user2=User(username="artyr663",email="art4453@mail.ru", password=generate_password_hash('1234'), is_staff=False, is_active=True)
+    print("ghghg")
+    session.add(user2)
+    session.commit()
+    return user2
+#create_users()
---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from auth_routers import auth_router
+from order_routers import order_router
+import uvicorn
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from models import User
+
+
+
+app=FastAPI()
+app.include_router(auth_router)
+app.include_router(order_router)
+
+secret_user: str = "newphone"
+
+secret_password: str = "whodis?"
+
+basic: HTTPBasicCredentials = HTTPBasic()
+@app.get("/who")
+def get_user(creds: HTTPBasicCredentials = Depends(basic)) -> dict:
+    if (creds.username == secret_user 
+        and creds.password == secret_password 
+        ):
+        return {"username": creds.username, "password": creds.password}
+    raise HTTPException(status_code=401, detail="Hey!")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
